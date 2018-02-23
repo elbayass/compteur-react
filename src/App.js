53a84445442eb9abe90a6cd00e0a0230ede8cc55
yassine <elbayass59@gmail.com>
2018-02-23 16:41:16 +0100

    exercice fini

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Welcome from './Welcome';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Welcome name="Yassine"/>
+    );
+  }
+}
+
+export default App;
